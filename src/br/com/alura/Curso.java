494363f777adcdba480ea40e0e301ba059d427a2
Leonardo Reis <leonardoreissousa95@gmail.com>
2019-02-11 11:37:40 -0200

    Subindo projeto para o github

---
+++ src/br/com/alura/Curso.java
@@ -0,0 +1,57 @@
+package br.com.alura;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Curso {
+
+	private String nome;
+	private String instrutor;
+	private List<Aula> aulas = new ArrayList<Aula>();
+	private Set<Aluno> alunos = new HashSet<Aluno>();
+
+	public Curso(String nome, String instrutor) {
+		super();
+		this.nome = nome;
+		this.instrutor = instrutor;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getInstrutor() {
+		return instrutor;
+	}
+
+	public List<Aula> getAulas() {
+		return Collections.unmodifiableList(aulas);
+	}
+
+	public void adiciona(Aula aula) {
+		this.aulas.add(aula);
+	}
+
+	public int getTempoTotal() {
+		int tempoTotal = 0;
+		for (Aula aula : aulas) {
+			tempoTotal += aula.getTempo();
+		}
+		return tempoTotal;
+	}
+
+	public void matricula(Aluno aluno) {
+		this.alunos.add(aluno);
+	}
+	public Set<Aluno> getAlunos() {
+		return Collections.unmodifiableSet(alunos);
+	}
+
+	public boolean estaMatriculado(Aluno aluno) {
+		return this.alunos.contains(aluno);
+	}
+	
+}
